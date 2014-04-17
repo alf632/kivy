2e4e3552c38c8000c8a7116fb755c9023f702839
Malte <malte.alfing@adition.com>
2014-04-17 19:30:54 +0200

    GRAVITATION :)

--- bahn.py
+++ bahn.py
@@ -14,7 +14,7 @@ class BounceBall(Widget):
     velocity_x = NumericProperty(randint(-2,2))
     velocity_y = NumericProperty(randint(-2,2))
     velocity = ReferenceListProperty(velocity_x, velocity_y)
-
+    neighbours=[]
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
@@ -25,7 +25,7 @@ class BahnGame(Widget):
     def add_ball(self,instance):
 	self.add_widget(BounceBall(pos=self.center,velocity_x=randint(-2,2), velocity_y=randint(-2,2)))
     def update(self, dt):
-	self.debug.text = str(self.children)
+	self.find_neighbours()
 
 	for bball in self.children:
             if "BounceBall object" in str(bball):
@@ -35,7 +35,30 @@ class BahnGame(Widget):
                 if bball.right > self.boundry.right or bball.center_x-(bball.right-bball.center_x) < self.boundry.center_x-(self.boundry.right-self.boundry.center_x):
                         bball.velocity_x *= -1
                 bball.move()
-#	self.ball.move()
+    def find_neighbours(self):
+	for bball in self.children:
+	    bball.neighbours=[]
+	    if "BounceBall object" in str(bball):
+		for obball in self.children:
+		    if "BounceBall object" in str(obball):
+			if str(bball) != str(obball):
+			    self.debug.text=str(bball.neighbours)
+                            if Vector(bball.center).distance(obball.center) < 50:
+				pass
+#				bball.velocity_x=1
+#				bball.velocity_y=1
+			    elif Vector(bball.center).distance(obball.center) < 100:
+				if bball.center_y > obball.center_y:
+				    bball.velocity_y += 1
+				elif bball.center_y < obball.center_y:
+				    bball.velocity_y += -1
+
+				if bball.center_x > obball.center_x:
+				    bball.velocity_x += 1
+                                elif bball.center_x < obball.center_x:
+                                    bball.velocity_x += -1
+
+				
 
 class BahnApp(App):
     def build(self):
@@ -43,7 +66,7 @@ class BahnApp(App):
 	but1 = Button(text='#')
 	but1.bind(on_press=game.add_ball)
 	game.add_widget(but1)
-	Clock.schedule_interval(game.update, 1.0/30.0)
+	Clock.schedule_interval(game.update, 1.0/15.0)
         return game
 
 
